adf0bbd94db9cdd6e8bcff3f4a42316ec5d68ef4
Alabi Tosin <alabitosin60@gmail.com>
2022-10-30 04:39:44 +0100

    - adds PostDetailsView method to DRA.views - updates DRA.urls

--- DRA/urls.py
+++ DRA/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostsView
+from .views import PostsView, PostDetailsView
 
 urlpatterns = [
-    path('posts/', PostsView)
+    path('posts/', PostsView),
+    path('details/<int:pk>/', PostDetailsView)
 ]
--- DRA/views.py
+++ DRA/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from .serializers import PostSerializer
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 
@@ -20,3 +20,26 @@ def PostsView(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def PostDetailsView(request, pk):
+    try:
+        post = Post.objects.get(pk=pk)
+    except post.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(post, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        post.delete()
+        return HttpResponse(status=204)
+
